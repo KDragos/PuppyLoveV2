e3fa02aa392ad1b9d2bb563f58582dad54553a54
kdragos <kristindragos@gmail.com>
2015-01-31 13:33:01 -0700

    Added CSS Project

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Milestone 3 CSS Project</title>
+	<link rel="stylesheet" href="styles/normalize.css">
+	<link rel="stylesheet" href="styles/stylesheet.css">
+</head>
+<body>
+	<div class="top">
+		<div class="header">
+			<a href="" class="logo">Puppy Love</a>
+			<form>
+				<input type="text" id="search" placeholder="Search">
+				<button>Search</button>
+			</form>
+			<nav>
+				<a href="">Home</a>
+				<a href="">Services</a>
+				<a href="">Portfolio</a>
+				<a href="">About</a>
+				<a href="">Membership</a>
+				<a href="">Testimonials</a>
+				<a href="">FAQ</a>
+				<a href="">Contact</a>
+			</nav>
+		</div>
+		<div class="slogan">
+			<div>
+				<h2><strong>Great</strong> People, <strong>Better</strong> Dogs!</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia quibusdam molestias laudantium doloribus perferendis aut laborum rerum at ad veritatis animi sequi cupiditate corrupti magni libero, quidem magnam dicta eos.</p>
+			</div>
+		</div>
+	</div>
+	<div class="bottom">
+		<div class="infobox">
+			<div>
+					<h4>Raise a litter</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipscing elit, sed do eiusmod tempor incididunt ut labore</p>
+					<button>Get help</button>
+			</div>
+			<div >
+					<h4>Feed 'em right</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipscing elit, sed do eiusmod tempor incididunt ut labore</p>
+					<button>Get food</button>
+			</div>
+			<div>
+					<h4>Sell your pups</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipscing elit, sed do eiusmod tempor incididunt ut labore</p>
+					<button>Sell dogs to China</button>
+			</div>
+		</div>
+		<main>
+			<div class="featured">
+				<div>
+					<img src="images/smalldog1.png" alt="Ralf">
+					<h4>Ralf</h4>
+					<p>This old mut likes to roll in the dirt.</p>
+				</div>
+				<div>
+					<img src="images/smalldog2.png" alt="Max">
+					<h4>Max</h4>
+					<p>If there was ever a young pup that liked grass, it's Max.</p>
+				</div>
+				<div>
+					<img src="images/smalldog3.png" alt="Rudy">
+					<h4>Rudy</h4>
+					<p>Young and fierce, that's all I have to say.</p>
+				</div>
+			</div>
+
+			<div class="profile">
+				<div>
+					<img src="images/dog1.png" alt="Ralf">
+					<h4>Ralf</h4>
+					<p>This old mut likes to roll in the dirt.</p>
+				</div>
+				<div>
+					<img src="images/dog2.png" alt="Max">
+					<h4>Max</h4>
+					<p>If there was ever a young pup that liked grass, it's Max.</p>
+				</div>
+				<div>
+					<img src="images/dog3.png" alt="Rudy">
+					<h4>Rudy</h4>
+					<p>Young and fierce, that's all I have to say.</p>
+				</div>
+				<div>
+					<img src="images/dog1.png" alt="Ralf">
+					<h4>Ralf</h4>
+					<p>This old mut likes to roll in the dirt.</p>
+				</div>
+				<div>
+					<img src="images/dog2.png" alt="Max">
+					<h4>Max</h4>
+					<p>If there was ever a young pup that liked grass, it's Max.</p>
+				</div>
+				<div>
+					<img src="images/dog3.png" alt="Rudy">
+					<h4>Rudy</h4>
+					<p>Young and fierce, that's all I have to say.</p>
+				</div>
+			</div>
+		</main>
+	</div>
+	
+</body>
+</html>
